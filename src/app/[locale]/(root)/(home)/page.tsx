b1f998d82fcfbddcea957b8f395b1665a3ee6e34
khayrullayev2006 <xayrullayevogabek604@gmail.com>
2023-12-21 20:10:46 +0500

    fetch datas in home page

--- src/app/[locale]/(root)/(home)/page.tsx
+++ src/app/[locale]/(root)/(home)/page.tsx
@@ -1,9 +1,46 @@
-"use client"
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Banner from "@/components/shared/banner/banner";
 import Common from "@/components/shared/common/common";
+import { ProductType, GroupedProductsType } from "@/types";
+import axios from "axios";
 
 const Home = () => {
+  const [products, setProducts] = useState<ProductType[]>([]);
+  const [sortedProducts,setSortedProducts] = useState<GroupedProductsType[]>([])
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const { data } = await axios.get(
+          "https://dummyjson.com/products?limit=100"
+        );
+
+        setProducts(data.products);
+
+        const sortedProducts: GroupedProductsType[] = Object.values(
+          data.products.reduce((acc: any, product: ProductType) => {
+            const category = product.category;
+            if (!acc[category]) {
+              acc[category] = {
+                categoryName: category,
+                data: [product],
+              };
+            } else {
+              acc[category].data.push(product);
+            }
+            return acc;
+          }, {})
+        );
+
+        setSortedProducts(sortedProducts);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, []);
+
+
   return (
     <div className="px-36 max-[1300px]:px-5">
       <Banner />
--- src/types/index.ts
+++ src/types/index.ts
@@ -37,3 +37,21 @@ export interface CategoryTypes {
     }
   ];
 }
+
+export interface ProductType {
+  brand: string;
+  category: string;
+  description: string;
+  discountPercentage: number;
+  id: number;
+  images: string[];
+  price: number;
+  rating: number;
+  stock: number;
+  thumbnail: string;
+  title: string;
+}
+
+export interface GroupedProductsType {
+  [category: string]: ProductType[];
+}
